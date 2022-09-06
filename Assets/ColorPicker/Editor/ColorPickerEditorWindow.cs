8ce37f19e72f9b58190cd1fc6214d5d4dd327860
thebaske <thebaske@gmail.com>
2022-09-06 12:22:17 +0200

    Direct test

--- Assets/ColorPicker/Editor/ColorPickerEditorWindow.cs
+++ Assets/ColorPicker/Editor/ColorPickerEditorWindow.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -19,6 +21,8 @@ public class ColorPickerEditorWindow : EditorWindow
     private Texture2D atlas;
     private Vector2 scrolPos;
     public Material useMat;
+    [SerializeField]public float uvSelected;
+    [SerializeField]public int submeshIndexDirect;
     public static void OpenColorPickerEditorWindow ()
     {
         GetWindow<ColorPickerEditorWindow> ();
@@ -28,13 +32,14 @@ public class ColorPickerEditorWindow : EditorWindow
     
     private void OnGUI ()
     {
-        
 
+        uvSelected = EditorGUILayout.FloatField("uv", uvSelected, GUILayout.Width(250f), GUILayout.Height(25));
+        submeshIndexDirect = EditorGUILayout.IntField("sub", submeshIndexDirect, GUILayout.Width(250f), GUILayout.Height(25));
+        ModifyMeshUVDirect(0, submeshIndexDirect);
         DisplayControllButtons();
         atlas = EditorGUILayout.ObjectField(atlas, typeof(Texture2D), false) as Texture2D;
         if (textures is null || textures.Count <= 0) return;
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
-
         int counter = 0;
         while (counter < textures.Count)
         {
@@ -112,28 +117,86 @@ public class ColorPickerEditorWindow : EditorWindow
             List<int> indiciesOfSubmesn = new List<int>();
             mf.sharedMesh.GetIndices(indiciesOfSubmesn, submeshIndex_);
             mf.sharedMesh.GetTriangles(indiciesOfSubmesn, submeshIndex_);
-            for (int i = 0; i < indiciesOfSubmesn.Count; i++)
-            {
-                Debug.Log($"vertices {indiciesOfSubmesn[i]}");
-            }
             int endIndex = smDc.vertexCount;
-            Vector2[] uv = new Vector2[mf.sharedMesh.uv.Length];
-            float pixelValue = RemapValues(0, textures.Count - 1, 0f, 1f, counter);
+            Vector2[] uv = mf.sharedMesh.uv;
+            // float pixelValue = RemapValues(0, textures.Count - 1, 0f, 1f, counter);
+            float pixelValue = (float)counter / (float)atlas.width;
             Debug.Log($"Pixel value {pixelValue}");
-            for (int j = 0; j < uv.Length; j++)
+            int counter___ = 0;
+            for (int j = 0; j < mf.sharedMesh.vertices.Length; j++)
+            {
+                
+                if (indiciesOfSubmesn.Contains(j))
+                {
+                    uvSelected = pixelValue;
+                    counter++;
+                    
+                    uv[j] = new Vector2(pixelValue, 0f);
+                    // indiciesOfSubmesn.Remove(j);
+                }
+
+               
+                // else
+                // {
+                //     uvsConverted[j] = mf.sharedMesh.uv[j];
+                // }
+            }
+
+            mf.sharedMesh.uv = uv;
+            
+            renderer.sharedMaterial.mainTexture = atlas;
+            EditorUtility.SetDirty(Selection.activeGameObject);
+        }
+    }
+    private void ModifyMeshUVDirect(int counter, int submeshIndex_)
+    {
+        MeshRenderer renderer = Selection.activeGameObject.GetComponent<MeshRenderer>();
+        if (renderer != null)
+        {
+            renderer.sharedMaterial = useMat;
+            Material[] submeshMats = new Material[renderer.sharedMaterials.Length];
+            for (int i = 0; i < renderer.sharedMaterials.Length; i++)
+            {
+                submeshMats[i] = useMat;
+            }
+
+            renderer.sharedMaterials = submeshMats;
+            MeshFilter mf = Selection.activeGameObject.GetComponent<MeshFilter>();
+            SubMeshDescriptor smDc = mf.sharedMesh.GetSubMesh(submeshIndex_);
+            Debug.LogError($"index count = {smDc.indexCount}, start index {smDc.indexStart} base vertex {smDc.baseVertex}  first vertex {smDc.firstVertex} vertex count {smDc.vertexCount}");
+            List<int> indiciesOfSubmesn = new List<int>();
+            mf.sharedMesh.GetIndices(indiciesOfSubmesn, submeshIndex_);
+            // mf.sharedMesh.GetTriangles(indiciesOfSubmesn, submeshIndex_);
+            
+          
+            int endIndex = smDc.vertexCount;
+            // Vector2[] uv = new Vector2[mf.sharedMesh.uv.Length];
+            Vector3[] uv = mf.sharedMesh.vertices;
+            Vector2[] uvsConverted = mf.sharedMesh.uv;
+            // float pixelValue = RemapValues(0, textures.Count - 1, 0f, 1f, counter);
+            float pixelValue = counter / atlas.width;
+            // Debug.Log($"Pixel value {pixelValue}");
+            for (int j = 0; j < mf.sharedMesh.vertices.Length; j++)
             {
                 if (indiciesOfSubmesn.Contains(j))
                 {
-                    uv[j] = new Vector2(pixelValue, 0f);
+                    // uvSelected = pixelValue;
+                    float result = 0f;
+                    if (uvSelected < 0.125f)
+                    {
+                        
+                    }
+                    uvsConverted[j] = new Vector2(uvSelected, 1f);
                     indiciesOfSubmesn.Remove(j);
                 }
                 else
                 {
-                    uv[j] = mf.sharedMesh.uv[j];
+                    uvsConverted[j] = mf.sharedMesh.uv[j];
                 }
+                
             }
 
-            mf.sharedMesh.uv = uv;
+            mf.sharedMesh.uv = uvsConverted;
             
             renderer.sharedMaterial.mainTexture = atlas;
             EditorUtility.SetDirty(Selection.activeGameObject);
@@ -191,7 +254,7 @@ public class ColorPickerEditorWindow : EditorWindow
     private void PackTextures(TextureData[] datas)
     {
         
-        int atlasSizeX = datas.Length * 50;
+        int atlasSizeX = (datas.Length - 1) * 100;
         atlas = new Texture2D(atlasSizeX, 1);
         Color32[] atlasColors = atlas.GetPixels32();
         int colorCounter = 0;
